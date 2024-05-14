bd027c38a28a0162ca99b09d99a928ec85c4b3da
LapVB <vboxuser@WIN11>
2024-05-14 10:02:43 +0200

    Laboratorium8 W trakcie

---
+++ CSLab08/CSLab08.WpfApp/AddStudentWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using CSLab08.BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CSLab08.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for AddStudentWindow.xaml
+    /// </summary>
+    public partial class AddStudentWindow : Window
+    {
+        public Student Student { get; set; }
+        public AddStudentWindow(Student student = null)
+        {
+            InitializeComponent();
+            if (student != null)
+            {
+                
+            }
+        }
+    }
+}
---
+++ CSLab08/CSLab08.WpfApp/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using CSLab08.BLL;
+
+namespace CSLab08.WpfApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public IList<Student> Students { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            Students = new List<Student>
+            {
+                new Student(){FirstName = "Jan", LastName="Kowalski"},
+                new Student(){FirstName= "Michal", LastName="Nowak"}
+            };
+            DataGridStudents.Columns.Add(item: new DataGridTextColumn() { Header = "First Name", Binding = new Binding(path: "FirstName") });
+            DataGridStudents.Columns.Add(item: new DataGridTextColumn() { Header = "Last Name", Binding = new Binding(path: "LastName") });
+            DataGridStudents.AutoGenerateColumns = false;
+            DataGridStudents.ItemsSource= Students;
+        }
+
+        private void _AddStudent_Click(object sender, RoutedEventArgs e)
+        {
+            AddStudentWindow win = new AddStudentWindow();
+            win.Show();
+        }
+    }
+}
